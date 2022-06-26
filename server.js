d56f4303fea88a725cd37b55eb4b1e128b4e2e02
Ron Barak <ronbarak@algoplanner.io>
2022-06-26 14:40:09 +0300

    removed some code

--- server.js
+++ server.js
@@ -31,18 +31,9 @@ app.use(
 
 app.use("/api/auth", userRoutes);
 
-const __dirname = path.resolve();
-app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
-
-  app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html")));
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running");
-  });
-}
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
 
 app.use(notFound);
 
